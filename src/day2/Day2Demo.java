643a83fe4dc671f1421256bbe7e3fdc55be2cf0c
Shabnam Tebyanian <130015815+Shabnam361@users.noreply.github.com>
2023-04-10 18:59:22 -0400

    Update Day2Demo.java

--- src/day2/Day2Demo.java
+++ src/day2/Day2Demo.java
@@ -5,7 +5,10 @@ public class Day2Demo {
 	public void getInfo() {
 		System.out.println("Pushing code.....");
 	}
+	/*
 	...This is coming from other team member
+	*/
+	
 		String firstName = "John";
 
 }
